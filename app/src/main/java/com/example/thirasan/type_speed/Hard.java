0e7a691022a123fd03e1afccc1ff2762fa5c1fda
Thirasan <thirasan@Thirasans-MacBook-Pro.local>
2017-06-05 20:18:49 +0700

    Finish

--- app/src/main/java/com/example/thirasan/type_speed/Hard.java
+++ app/src/main/java/com/example/thirasan/type_speed/Hard.java
@@ -4,5 +4,15 @@ package com.example.thirasan.type_speed;
  * Created by thirasan on 6/5/2017 AD.
  */
 
-public class Hard {
+public class Hard implements Level {
+
+    String[] word = {"drive.google.com/drive/u/4/folders/0B4JvzTdt3eV2MFJUZjVIZk5mOG8",
+            "www.facebook.com/groups/1339396752802374/?fref=ts",
+            "www.goethe.de/ins/th/th/spr/kur/gia/tup.cfm",
+            "www.tafasu.com/watch?v=qfspnjeUD1","www.youtube.com/watch?v=1oAfHtgkB2k&t=2974s"};
+
+    @Override
+    public String getWord() {
+        return word[(int)(Math.random() * word.length)];
+    }
 }
